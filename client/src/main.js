768640082d4c964c0260167dec44e5b687e301a7
fafaop <86034548+fafaop@users.noreply.github.com>
2026-07-09 18:54:15 +0800

    feat: integrate client websocket startup

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
+const PetSocket = require('./network/websocket');
 
 let petState = {
   name: 'Mimi',
@@ -8,6 +9,8 @@ let petState = {
   hunger: 20,
   online: false
 };
+
+let socket;
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -24,17 +27,29 @@ function createWindow() {
 
   win.loadFile(path.join(__dirname, 'pet.html'));
 
+  socket = new PetSocket('ws://localhost:8080');
+  socket.on('WELCOME', msg => {
+    petState.online = true;
+    if (msg.pet) petState = { ...petState, ...msg.pet };
+  });
+  socket.on('PET_UPDATE', msg => {
+    if (msg.pet) petState = { ...petState, ...msg.pet };
+  });
+  socket.connect();
+
   ipcMain.handle('pet:getState', () => petState);
 
   ipcMain.handle('pet:feed', () => {
     petState.hunger = Math.max(0, petState.hunger - 20);
     petState.mood = Math.min(100, petState.mood + 10);
+    socket.send('PET_UPDATE', { state: petState });
     return petState;
   });
 
   ipcMain.handle('pet:play', () => {
     petState.mood = Math.min(100, petState.mood + 15);
     petState.hunger = Math.min(100, petState.hunger + 5);
+    socket.send('PET_UPDATE', { state: petState });
     return petState;
   });
 }
