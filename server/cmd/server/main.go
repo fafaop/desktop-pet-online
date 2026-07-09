1a8d2e8552055b7f3a4347897e0deb2135ef1b7a
fafaop <86034548+fafaop@users.noreply.github.com>
2026-07-09 14:41:04 +0800

    initialize server application entry

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -3,5 +3,6 @@ package main
 import "log"
 
 func main() {
-	log.Println("Desktop Pet Online server starting")
+	log.Println("Desktop Pet Online Server starting")
+	log.Println("Gateway listening on :8080")
 }
