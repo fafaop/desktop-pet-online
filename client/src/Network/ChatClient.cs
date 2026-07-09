43feb16463c89993804a37604e2fe6d7fdb6c919
fafaop <86034548+fafaop@users.noreply.github.com>
2026-07-09 13:43:56 +0800

    add client chat module skeleton

---
+++ client/src/Network/ChatClient.cs
@@ -0,0 +1,9 @@
+namespace DesktopPet.Network;
+
+public class ChatClient
+{
+    public void SendPrivateMessage(long userId, string content)
+    {
+        // TODO build CHAT_MESSAGE request
+    }
+}
