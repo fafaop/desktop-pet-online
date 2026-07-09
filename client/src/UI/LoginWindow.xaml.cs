4d762c1eee1ba5af4d75412693d7c58804ab9f32
fafaop <86034548+fafaop@users.noreply.github.com>
2026-07-09 15:55:05 +0800

    add login window code behind skeleton

---
+++ client/src/UI/LoginWindow.xaml.cs
@@ -0,0 +1,16 @@
+using System.Windows;
+
+namespace DesktopPet;
+
+public partial class LoginWindow : Window
+{
+    public LoginWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Login_Click(object sender, RoutedEventArgs e)
+    {
+        // TODO call NetworkManager login
+    }
+}
