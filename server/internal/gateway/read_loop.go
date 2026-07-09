24c6079f93d07f8b14790857a4da4fac95d1fbcb
fafaop <86034548+fafaop@users.noreply.github.com>
2026-07-09 13:45:30 +0800

    add websocket read loop skeleton

---
+++ server/internal/gateway/read_loop.go
@@ -0,0 +1,12 @@
+package gateway
+
+// ReadLoop receives messages from client connection.
+func ReadLoop(session *ClientSession) {
+	for {
+		// TODO:
+		// 1. read websocket frame
+		// 2. decode message
+		// 3. dispatch request
+		break
+	}
+}
